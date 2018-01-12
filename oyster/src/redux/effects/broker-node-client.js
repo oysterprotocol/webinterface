82ed1ffadfb824997b0070c0d4e6700182f98c1e
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-12 16:08:59 -0500

    Create effects folder

---
+++ oyster/src/redux/effects/broker-node-client.js
@@ -0,0 +1,9 @@
+/**
+ * NOTE: effects folder contains any interaction with the outside world,
+ * whether than be making HTTP requests, receiving notifications,
+ * playing sounds, etc. Because this is part of the app is least in out control,
+ *  all functions in this directory need to make sure all data being
+ * returned is properly checked and validated for the app to handle.
+ */
+
+// TODO: Put all interactions with backend here.
