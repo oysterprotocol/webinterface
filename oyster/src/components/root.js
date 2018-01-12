10b2d73c90785199e2b7ad748a9a5ec34a3885e2
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-12 14:33:32 -0500

    [WIP] First pass to connect redux to react

---
+++ oyster/src/components/root.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Logo from "components/shared/Logo";
+import "components/App.css";
+
+const Root = () => (
+  <div className="App">
+    <header className="App-header">
+      <Logo />
+      <h1 className="App-title">Welcome to Oyster</h1>
+    </header>
+    <button className="App-intro">Upload a file.</button>
+  </div>
+);
+
+export default Root;
--- oyster/src/index.js
+++ oyster/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import "./index.css";
 
-import App from "./components/App";
+import redux from "./redux";
+import Root from "./components/root";
 import registerServiceWorker from "./registerServiceWorker";
+
+const App = () => (
+  <Provider store={redux}>
+    <Root />
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
