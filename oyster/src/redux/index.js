dcc1a8b3636b202a6b19a38fd81050850c5c9da9
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-12 16:19:36 -0500

    Installs redux-logger

--- oyster/src/redux/index.js
+++ oyster/src/redux/index.js
@@ -1,11 +1,15 @@
 import { applyMiddleware, compose, createStore } from "redux";
+import { createLogger } from "redux-logger";
 import { createEpicMiddleware } from "redux-observable";
 
 import epics from "./epics";
 import reducer from "./reducers";
 
 const composeFn = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = [createEpicMiddleware(epics)];
+const middleware = [
+  process.env.NODE_ENV === `development` && createLogger(),
+  createEpicMiddleware(epics)
+];
 const store = createStore(reducer, composeFn(applyMiddleware(...middleware)));
 
 export default store;
