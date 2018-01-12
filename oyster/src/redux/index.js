3e1ae08143ab21ada00ba5b9e4b2e65c38590c3b
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-12 16:10:31 -0500

    Integrate redux dev tools with redux.

--- oyster/src/redux/index.js
+++ oyster/src/redux/index.js
@@ -4,7 +4,8 @@ import { createEpicMiddleware } from "redux-observable";
 import epics from "./epics";
 import reducer from "./reducers";
 
+const composeFn = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [createEpicMiddleware(epics)];
-const store = createStore(reducer, compose(applyMiddleware(...middleware)));
+const store = createStore(reducer, composeFn(applyMiddleware(...middleware)));
 
 export default store;
