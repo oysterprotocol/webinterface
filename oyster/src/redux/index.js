0867b082415c4792c714f9750d73eeed73080162
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-12 16:02:56 -0500

    Installs redux-observables

--- oyster/src/redux/index.js
+++ oyster/src/redux/index.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import { createEpicMiddleware } from "redux-observable";
 
+import epics from "./epics";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const middleware = [createEpicMiddleware(epics)];
+const store = createStore(reducer, compose(applyMiddleware(...middleware)));
 
 export default store;
