1459cd52a6c805b408798adc542bc8fa97e66f63
Edmund Mai <edmundmai@gmail.com>
2018-01-17 15:52:50 -0500

    remove comment

--- oyster/src/redux/epics/file-epic.js
+++ oyster/src/redux/epics/file-epic.js
@@ -5,7 +5,6 @@ import fileActions from "redux/actions/file-actions";
 
 import FileProcessor from "utils/file-processor";
 
-// TODO: Firebase can handle syncing through pushes. No need to pull these.
 function uploadFile(action$, store) {
   return action$.ofType(fileActions.UPLOAD).mergeMap(action => {
     const file = action.payload;
