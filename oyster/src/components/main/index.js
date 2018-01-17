71b31763a56c99db46ae5d638855bcbd2bd6cadf
Edmund Mai <edmundmai@gmail.com>
2018-01-17 15:36:39 -0500

    remove example redux stuff

--- oyster/src/components/main/index.js
+++ oyster/src/components/main/index.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import exampleActions from "../../redux/actions/example-actions";
 import fileActions from "../../redux/actions/file-actions";
 
 const mapStateToProps = state => ({
-  example: state.example,
   file: state.file
 });
 const mapDispatchToProps = dispatch => ({
-  exampleFn: () => dispatch(exampleActions.incrementAction()),
   uploadFn: file => dispatch(fileActions.uploadAction(file))
 });
 
--- oyster/src/redux/reducers/index.js
+++ oyster/src/redux/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
 
-import example from "redux/reducers/example-reducer";
+import file from "redux/reducers/file-reducer";
 
-export default combineReducers({ example });
+export default combineReducers({ file });
