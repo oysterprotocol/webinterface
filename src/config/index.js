c09d04cee5b0dd123f80c27fd89119d8921a1f56
Edmund Mai <edmundmai@gmail.com>
2018-01-29 15:34:37 -0500

    readd

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 export const API = Object.freeze({
   HOST: "http://localhost:8000",
-  BROKER_NODE_A: "http://localhost:8000",
-  BROKER_NODE_B: "http://localhost:8000",
-  // BROKER_NODE_A: "https://broker-1.oysternodes.com",
-  // BROKER_NODE_B: "https://broker-2.oysternodes.com",
+  // BROKER_NODE_A: "http://localhost:8000",
+  // BROKER_NODE_B: "http://localhost:8000",
+  BROKER_NODE_A: "https://broker-1.oysternodes.com",
+  BROKER_NODE_B: "https://broker-2.oysternodes.com",
   V1_UPLOAD_SESSIONS_PATH: "/api/v1/upload-sessions"
 });
 
