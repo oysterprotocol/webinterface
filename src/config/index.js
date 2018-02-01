12539519e213fa8ba9bdf882cb13c6660e5e295a
Edmund Mai <edmundmai@gmail.com>
2018-02-01 16:06:57 -0500

    when downloading file, fetch from 3 different iota nodes

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,9 @@ export const API = Object.freeze({
 });
 
 export const IOTA_API = Object.freeze({
-  PROVIDER: "http://eugene.iota.community:14265",
+  PROVIDER_A: "http://eugene.iota.community:14265",
+  PROVIDER_B: "http://eugene.iotasupport.com:14999",
+  PROVIDER_C: "http://eugeneoldisoft.iotasupport.com:14265",
   ADDRESS_LENGTH: 81
 });
 
