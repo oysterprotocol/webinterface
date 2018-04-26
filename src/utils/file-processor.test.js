feddb41b5335b8c1de448b9a6cf4e66f4cf5eb27
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-04-26 18:12:10 -0400

    More robust tests

--- src/utils/file-processor.test.js
+++ src/utils/file-processor.test.js
@@ -4,7 +4,7 @@ import FileProcessor from "./file-processor";
 
 test("file |> fileToChunks |> chunksToFile - Success", done => {
   const handle = "super-secret-key";
-  const ogFileContents = "test123-".repeat(2187);
+  const ogFileContents = "test123-".repeat(5000);
   const file = new File([ogFileContents], "testFile.txt");
 
   FileProcessor.fileToChunks(file, handle)
@@ -22,18 +22,18 @@ test("file |> fileToChunks |> chunksToFile - Success", done => {
 
 test("file |> fileToChunks |> chunksToFile - Success w/ treasure", done => {
   const handle = "super-secret-key";
-  const ogFileContents = "test123-".repeat(1);
+  const ogFileContents = "test123-".repeat(5000);
   const file = new File([ogFileContents], "testFile.txt");
-  const numTreasure = 5;
+  const numTreasure = 100;
 
   FileProcessor.fileToChunks(file, handle)
     .then(encryptedChunks => {
-      // Insert treasure into randomly into chunks.
+      // Insert treasure randomly into chunks.
       for (let i = 0; i < numTreasure; i++) {
-        let treasureIdx = Math.floor(Math.random() * encryptedChunks.length);
+        let maxIdx = encryptedChunks.length + i;
+        let treasureIdx = Math.floor(Math.random() * maxIdx);
         encryptedChunks.splice(treasureIdx, 0, "TREASURE");
       }
-      console.log(encryptedChunks);
 
       return FileProcessor.chunksToFile(encryptedChunks, handle);
     })
