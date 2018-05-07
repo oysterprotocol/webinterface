54bd5f1ae4f1ec50e10d8ee170518ff02ba3bc64
Jared Farago <jared.farago@gmail.com>
2018-05-07 10:05:42 -0400

    Added retention years alert if set to 0

--- src/components/upload-form/upload-slide.js
+++ src/components/upload-form/upload-slide.js
@@ -164,6 +164,10 @@ class UploadSlide extends Component {
                 alert(
                   `Please select a file under ${FILE.MAX_FILE_SIZE / 1000} KB.`
                 );
+              } else if (retentionYears == 0) {
+                alert(
+                  `Please select retention years`
+                );
               } else {
                 upload(file);
               }
