c594f662108bbe4fda5ff93671664e2216e04c6d
Jared Farago <jared.farago@gmail.com>
2018-05-14 13:22:12 -0400

    - Added payment confirmation spinner

--- src/components/payment-confirm/payment-confirm-slide.js
+++ src/components/payment-confirm/payment-confirm-slide.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Slide from "components/shared/slide";
 import ICON_READY from "assets/images/icon_ready.png";
+import ICON_SPINNER from "assets/images/icon_spinner.png";
 
 
 const PaymentConfirmSlide = () => (
   <Slide title="Transaction Received" image={ICON_READY}>
     <p>Your transaction has been received, and is now being confirmed on the Ethereum Blockchain.</p>
+    <img src={ICON_SPINNER} className="payment-confirm-spinner spin-2s"/>
   </Slide>
 );
 
