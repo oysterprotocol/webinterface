4e4792f766734e91b3f4e6226be003c331f0c965
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-05-25 11:52:18 -0400

    Qa (#126)
    * Auto configure webinterface to point to dev brokernodes when running in development mode
    
    * Default to include treasure offsets
    
    * copy paste fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,12 @@
+const IS_DEV = process.env.NODE_ENV === "development";
+
+// TODO: Script to set produciton values.
+const ALPHA_IP = IS_DEV ? "52.14.218.135" : "ALPHA_IP";
+const BETA_IP = IS_DEV ? "18.217.133.146" : "BETA_IP";
+
 export const API = Object.freeze({
-  HOST: "http://ALPHA_BROKER_IP",
-  BROKER_NODE_A: "http://ALPHA_BROKER_IP",
-  BROKER_NODE_B: "http://BETA_BROKER_IP",
+  BROKER_NODE_A: `http://${ALPHA_IP}`,
+  BROKER_NODE_B: `http://${BETA_IP}`,
   V1_UPLOAD_SESSIONS_PATH: ":3000/api/v1/upload-sessions",
   V2_UPLOAD_SESSIONS_PATH: ":3000/api/v2/upload-sessions",
   GAS_PRICE: "https://api.blockcypher.com/v1/eth/main",
@@ -9,9 +14,9 @@ export const API = Object.freeze({
 });
 
 export const IOTA_API = Object.freeze({
-  PROVIDER_A: "http://ALPHA_BROKER_IP:14265",
-  PROVIDER_B: "http://ALPHA_BROKER_IP:14265",
-  PROVIDER_C: "http://ALPHA_BROKER_IP:14265",
+  PROVIDER_A: `http://${ALPHA_IP}:14265`,
+  PROVIDER_B: `http://${BETA_IP}:14265`,
+  PROVIDER_C: `http://${ALPHA_IP}:14265`,
   ADDRESS_LENGTH: 81,
   MESSAGE_LENGTH: 2187,
   BUNDLE_SIZE: 100
@@ -38,7 +43,7 @@ export const FILE = Object.freeze({
   }
 });
 
-export const INCLUDE_TREASURE_OFFSETS = false;
+export const INCLUDE_TREASURE_OFFSETS = true;
 export const NUM_BROKER_CHANNELS = 3; // number of broker cores - 1
 export const SECONDS_PER_CHUNK = 0.035; // if MWM is changed this will be incorrect
 export const MAX_ADDRESSES = 1000;
