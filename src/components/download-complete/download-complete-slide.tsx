b642c96a36fb125ec492d46f89ec97c376294dbc
Jared Farago <jared.farago@gmail.com>
2018-08-08 12:28:53 -0400

    Converted download-complete-slide to a class

--- src/components/download-complete/download-complete-slide.tsx
+++ src/components/download-complete/download-complete-slide.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import SlideContainer from "../shared/slide";
 
-const DownloadCompleteSlide = () => (
-  <SlideContainer title="Your download is ready">
-    <div>
-      <p>Click the button below to begin downloading your file.</p>
-      <button className="btn btn-default">DOWNLOAD</button>
-    </div>
-  </SlideContainer>
-);
+class DownloadCompleteSlide extends React.Component {
+    render() {
+        return (
+            <SlideContainer title="Your download is ready">
+                <div>
+                    <p>Click the button below to begin downloading your file.</p>
+                    <button className="btn btn-default">DOWNLOAD</button>
+                </div>
+            </SlideContainer>
+        )
+    }
+}
 
 export default DownloadCompleteSlide;
