3bef692fb13159316137427bc03916669e1f16a2
Edmund Mai <edmundmai@gmail.com>
2018-10-10 14:30:02 -0400

    upload complete slide

--- src/components/upload-complete/upload-complete-slide.tsx
+++ src/components/upload-complete/upload-complete-slide.tsx
@@ -1,24 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 import ClipboardBtn from "../shared/clipboard-button";
 import Slide from "../shared/slide";
+
+const HandleHeader = styled.span`
+  color: #afcbfe;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const OysterHandle = styled.p`
+  color: #0068ea;
+  font-size: 15px;
+`;
 
 const ICON_READY = require("../../assets/images/icon_ready.png");
 
 const UploadCompleteSlide = ({ handle }) => (
   <Slide title="Upload Complete" image={ICON_READY}>
-    <p className="complete-instructions">
+    <p>
       Your file has been successfully uploaded to the Tangle. An Oyster handle
       has been generated below. This handle is the only way to access your file
       on the Tangle. Please store this handle in a safe place.
     </p>
     <div>
       <p>
-        <span className="handle-header">Oyster Handle:</span>
+        <HandleHeader>Oyster Handle:</HandleHeader>
       </p>
-      <p id="oyster-handle" className="oyster-handle">
-        {handle}
-      </p>
+      <OysterHandle id="oyster-handle">{handle}</OysterHandle>
       <ClipboardBtn text={handle}>Copy to clipboard</ClipboardBtn>
     </div>
   </Slide>
